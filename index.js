fcdbf2ba1b81802fb3838dd7960688c0a7a00895
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-08 14:27:05 +0300

    commit tree c6ccc7a4a3975011704d86086fa871c4a47d2a1c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer(){
+   bestCustomer = "not bob"
+  
+}
+function overwriteBestCustomer(){
+  bestCustomer ="maybe bob"
+
+}
+const leastFavoriteCustomer ="tonny"
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "mike"
+}
